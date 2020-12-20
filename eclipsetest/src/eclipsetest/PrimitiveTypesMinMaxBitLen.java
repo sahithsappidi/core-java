80c5c9320c8a220117af47dbdb147710c7b08fa2
sahith sappidi <sahithreddysappidi@gmail.com>
2020-12-20 17:24:20 -0600

    primitives min max kength

---
+++ eclipsetest/src/eclipsetest/PrimitiveTypesMinMaxBitLen.java
@@ -0,0 +1,47 @@
+package eclipsetest;
+
+public class PrimitiveTypesMinMaxBitLen {
+
+	public static void main(String[] args) {
+		//INTEGER
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.SIZE);
+		
+		//BYTE
+		System.out.println(Byte.MIN_VALUE);
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.SIZE);
+		
+		//SHORT
+		
+		System.out.println(Short.MIN_VALUE);
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.SIZE);
+		
+		//LONG
+		
+		System.out.println(Long.MIN_VALUE);
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.SIZE);
+		
+		//CHAR
+		System.out.println(Character.MIN_VALUE);
+		System.out.println(Character.MAX_VALUE);
+		System.out.println(Character.SIZE);
+		
+		//FLOAT
+		System.out.println(Float.MIN_VALUE);
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.SIZE);
+		
+		//DOUBLE
+		System.out.println(Double.MIN_VALUE);
+		System.out.println(Double.MAX_VALUE);
+		System.out.println(Double.SIZE);
+		
+		
+		
+	}
+
+}
