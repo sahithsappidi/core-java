dc40e8dfd4fc6a126114e2d09e2409fac1b75f1c
sahith sappidi <sahithreddysappidi@gmail.com>
2020-12-20 18:08:16 -0600

    esc sequences

---
+++ eclipsetest/src/eclipsetest/EscSequence.java
@@ -0,0 +1,14 @@
+package eclipsetest;
+
+public class EscSequence {
+
+	public static void main(String[] args) {
+		System.out.println("\t \'_\'");
+		System.out.println("\t (oo)");
+		System.out.println("  +========\\/");
+		System.out.println(" / || %%% ||");
+		System.out.println("* || ----- ||");
+		System.out.println("  \"\"       \"\"");	
+		}
+
+}
