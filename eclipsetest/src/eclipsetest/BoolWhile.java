b0b99bb926d7e23c812672eb86bfe57e49390550
sahith sappidi <sahithreddysappidi@gmail.com>
2020-12-23 22:49:42 -0600

    12/23/20

---
+++ eclipsetest/src/eclipsetest/BoolWhile.java
@@ -0,0 +1,26 @@
+package eclipsetest;
+
+import java.util.Scanner;
+
+public class BoolWhile {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		boolean isValid;
+		int number=in.nextInt();
+		do {
+			if(number>=1 && number<=10) {
+				isValid=true;
+			}
+			else {
+				isValid=false;
+			}
+		}
+		while(!isValid);
+		System.out.println(number);
+		//in.close();
+	}
+	
+
+}
